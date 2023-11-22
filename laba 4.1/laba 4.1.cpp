eff2e0971256ea3953e16ba5bc5fc225ddfdfc0f
s8983 <s8983@DESKTOP-HFC69N8>
2023-11-22 15:28:35 +0700

    Добавьте файлы проекта.

---
+++ laba 4.1/laba 4.1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <locale>
+
+using namespace std;
+
+/*
+ * Лабораторная работа 4. Задание 1. Индивидуальный вариант 24.
+ * Посчитать четные и нечетные цифры введенного натурального числа
+ */
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    int number;
+    int digit;
+    int even = 0;
+    int odd = 0;
+
+    cout << "Введите натуральное число ";
+    cin >> number;
+
+    while (number != 0) {
+        digit = number % 10;
+        if (digit % 2 == 0) {
+            even++;
+        }
+        else {
+            odd++;
+        }
+
+        number /= 10;
+    }
+    cout << "Количество четных цифр: " << even << endl;
+    cout << "Количество нечетных цифр: " << odd << endl;
+}
